cbf30900a9aa6195aed612303cd20a91aa0550bf
SAHIL YADAV <sahil952003@gmail.com>
2024-07-10 04:45:31 +0530

    COMMIT

---
+++ config.js
@@ -0,0 +1,12 @@
+
+var config = {
+    lines: [
+        "Dear Samrudhi,",
+        "i never knew how much you mean to me when we first started talking . until today,i still like u as much as i did since the very first day we met. ",
+        " every since you came into my life, i felt like i was the happiest. i just want to let you know how much you mean to me and how much i appreciate you.",
+        "you never failed to make me smile.  ", 
+       
+    ],
+    date:"2024-05-25",
+    names: ["☃︎【 Your Sahil 】","‎ "]
+}
